d96caa7ece3c5ad91722c7073d71898c9d4cad98
nifanic <nifanic@masterpage.net>
2020-04-11 21:14:32 -0400

    Add types to `<Ratio />`

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import { Ratio } from "./Ratio";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <Ratio>
+      {(width, height, hasComputed) =>
+        hasComputed ? (
+          <span>
+            width: {width}, height: {height}
+          </span>
+        ) : <span>Not ready.</span>
+      }
+    </Ratio>
+  );
 }
 
 export default App;
--- src/Ratio.js
+++ src/Ratio.js
@@ -1,61 +1,86 @@
 import React from "react";
 
+/**
+ * @typedef {Object} RatioProps
+ * @property {number} x
+ * @property {number} y
+ */
+/**
+ * @typedef {Object} RatioState
+ * @property {boolean} [hasComputed=false]
+ */
+/**
+ * @typedef {Object} RatioShape
+ * @property {number} [heigth=0]
+ * @property {number} [width=0]
+ */
+
 export class Ratio extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hasComputed: false,
-      width: 0,
-      height: 0
-    };
-  }
-  
-  getComputedDimensions({ x, y }) {
+  /** @type {RatioShape & RatioState} */
+  state = {
+    hasComputed: false,
+    width: 0,
+    height: 0
+  };
+
+  /**
+   * @param {RatioProps | React.ReactNode} props
+   * @return {RatioShape}
+   */
+  getComputedDimensions = props => {
+    const { x, y } = props;
     const { width } = this.container.getBoundingClientRect();
-    
+
     return {
       width,
       heigth: width * (y / x)
     };
-  }
-  
-  componentWillReceiveProps(nextProps) {
+  };
+
+  /**
+   * @param {RatioProps} nextProps
+   * @param nextContext
+   */
+  UNSAFE_componentWillReceiveProps(nextProps, nextContext) {
     this.setState(this.getComputedDimensions(nextProps));
   }
-  
+
   componentDidMount() {
     this.setState({
       ...this.getComputedDimensions(this.props)
     });
-    
+
     window.addEventListener("resize", this.handleResize, false);
   }
-  
+
   componentWillUnmount() {
     window.removeEventListener("resize", this.handleResize, false);
   }
-  
+
   handleResize = () => {
     this.setState(
       {
         hasComputed: false
       },
-      () => {}
+      () => {
+        this.setState({
+          hasComputed: true,
+          ...this.getComputedDimensions(this.props)
+        });
+      }
     );
-    this.setState({
-      hasComputed: true,
-      ...this.getComputedDimensions(this.props)
-    });
   };
-  
+
   render() {
+    /** @type {{RatioProps, RatioState}} */
+    const {
+      props: { children },
+      state: { width, height, hasComputed }
+    } = this;
+
     return (
       <div ref={ref => (this.container = ref)}>
-        {this.props.children(
-          this.state.width,
-          this.state.height,
-          this.state.hasComputed
-        )}
+        {children(width, height, hasComputed)}
       </div>
     );
   }
